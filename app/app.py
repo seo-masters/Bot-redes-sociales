298c151deadfbddfef116dcd40a112e38bbeafa9
programadorAzteca03 <mdlcsanchez@yahoo.com>
2023-10-10 12:52:51 -0500

    xxx

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from gui.main_window import MiVentanaPrincipal
 class MiAplicacion:
     def __init__(self, root):
         self.root = root
-        self.root.title("Bot Redes")
+        self.root.title("Bot Redes xxxxx")
         self.root.geometry("400x300")
         
         self.ventana_principal = MiVentanaPrincipal(root)
